cb0eee7b871e6f15da58cc060d5bf5236c7da1f7
Birgit <birgit.bartenschlager@web.de>
2023-07-30 15:36:49 +0200

    style changes

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ document
     "click",
     () => (document.getElementById("nav").style.display = "none")
   );
+
+//   GSAP ANIMATION
+
+var tl = gsap.timeline({ defaults: { duration: 1 } });
+
+tl.from(".main-copy", { y: 50, opacity: 0 })
+  .to(
+    "h1 span",
+    { clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)" },
+    "-=.7"
+  )
+  .from("ul.featured-cabins li", { y: 50, opacity: 0, stagger: 0.3 }, "-=.7");
