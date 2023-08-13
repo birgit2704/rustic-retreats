1802c19b37b6f2a146002f400f0ec64f2e68dabd
Birgit <birgit.bartenschlager@web.de>
2023-08-13 10:28:08 +0200

    design cabin info page

---
+++ data.js
@@ -0,0 +1,112 @@
+const cabins = [
+  {
+    id: "0001",
+    name: "Serene Silverbrook",
+    image: "imgs/cabin2.jpg",
+    pricePerNight: "51 EUR",
+    shortDescription: "Tranquil cabin getaway in lush woods",
+    longDescription:
+      "Step into an enchanting cabin nestled in glimmering glades, a place where the magic of the surroundings blends seamlessly with the soothing presence of nature.",
+    country: "Canada",
+    area: "British Columbia",
+    maxNoGuests: 6,
+  },
+  {
+    id: "0002",
+    name: "Whispering Willows",
+    image: "imgs/cabin1.jpg",
+    pricePerNight: "83 EUR",
+    shortDescription: "Secluded nature retreat among whispering trees",
+    longDescription:
+      "Immerse yourself in the embrace of nature's beauty, with a cabin surrounded by whispering trees that invite you to unwind and reconnect with the world around you.",
+    country: "USA",
+    area: "Smoky Mountains",
+    maxNoGuests: 5,
+  },
+  {
+    id: "0003",
+    name: "Cozy Cedarwoods",
+    image: "imgs/cabin3.jpg",
+    pricePerNight: "64 EUR",
+    shortDescription: "Cozy cedar haven for a peaceful escape",
+    longDescription:
+      "Find solace in a cozy cedar haven, where fragrant woods envelop you in warmth, providing a serene escape from the bustling city life.",
+    country: "Norway",
+    area: "Telemark",
+    maxNoGuests: 4,
+  },
+  {
+    id: "0004",
+    name: "Rustling Redwoods",
+    image: "imgs/cabin4.jpg",
+    pricePerNight: "57 EUR",
+    shortDescription: "Elegant redwood retreat in a serene setting",
+    longDescription:
+      "Discover a hidden gem amidst lush woods, offering unparalleled tranquility and utmost comfort for the perfect getaway.",
+    country: "USA",
+    area: "Northern California",
+    maxNoGuests: 7,
+  },
+  {
+    id: "0005",
+    name: "Glimmering Glades",
+    image: "imgs/cabin5.jpg",
+    pricePerNight: "52 EUR",
+    shortDescription: "Enchanting glade cabin with magical ambiance",
+    longDescription:
+      "An elegant retreat awaits among towering redwoods, promising not just relaxation, but an immersion in the lap of natural beauty itself.",
+    country: "France",
+    area: "Provence",
+    maxNoGuests: 6,
+  },
+  {
+    id: "0006",
+    name: "Tranquil Timberland",
+    image: "imgs/cabin6.jpg",
+    pricePerNight: "78 EUR",
+    shortDescription: "Timberland sanctuary for relaxation and peace",
+    longDescription:
+      "Experience a tranquil timberland retreat that serves as a cozy sanctuary, a place to connect deeply with the wilderness and find rejuvenation in the midst of trees and serenity.",
+    country: "Sweden",
+    area: "Dalarna",
+    maxNoGuests: 8,
+  },
+  {
+    id: "0007",
+    name: "Meadow Melodies",
+    image: "imgs/cabin7.jpg",
+    pricePerNight: "50 EUR",
+    shortDescription: "Charming woodland cabin with meadow views",
+    longDescription:
+      "Revel in the symphony of meadow melodies and gentle breezes that grace this charming woodland cabin, a place where the beauty of the surroundings is matched only by the comfort within.",
+    country: "New Zealand",
+    area: "South Island",
+    maxNoGuests: 5,
+  },
+  {
+    id: "0008",
+    name: "Cozy Creekview",
+    image: "imgs/cabin8.jpg",
+    pricePerNight: "59 EUR",
+    shortDescription: "Riverside escape in the heart of nature",
+    longDescription:
+      "Embark on a riverside escape that offers you a front-row seat to the harmonious dance of nature. Cozy comforts combine with the grandeur of the outdoors, creating an experience that truly embodies the essence of tranquility.",
+    country: "USA",
+    area: "Blue Ridge Mountains",
+    maxNoGuests: 6,
+  },
+  {
+    id: "0009",
+    name: "Dancing Daisies",
+    image: "imgs/cabin9.jpg",
+    pricePerNight: "68 EUR",
+    shortDescription: "Delightful cabin surrounded by dancing daisies",
+    longDescription:
+      "Finally, find yourself surrounded by dancing daisies in this delightful cabin, where joy and calm effortlessly converge, creating a space that feels both invigorating and peaceful.",
+    country: "Netherlands",
+    area: "Veluwe",
+    maxNoGuests: 7,
+  },
+];
+
+export default cabins;
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import cabins from "./data.js";
+
+// NAVIGATION
+
 document
   .getElementById("menu-btn")
   .addEventListener(
@@ -22,4 +26,61 @@ tl.from(".main-copy", { y: 50, opacity: 0 })
     { clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)" },
     "-=.7"
   )
-  .from("ul.featured-cabins li", { y: 50, opacity: 0, stagger: 0.3 }, "-=.7");
+  .from("ul.featured-cabins", { y: 50, opacity: 0, stagger: 0.3 }, "-=.7");
+
+// RENDER CABINS IN INDEX.HTML
+const threeCabins = document.getElementById("three-featured-cabins");
+
+if (threeCabins) {
+  const threeCabinsHtml = cabins
+    .slice(0, 3)
+    .map(({ image, name, longDescription }) => {
+      return `
+  <li>
+    <a href="#"
+      ><img src="${image}" class="cabin-img" alt="cabin"
+    /></a>
+    <p class="cabin-title">${name}</p>
+    <p class="cabin-desc">
+    ${longDescription}
+    </p>
+
+    <a href="#" class="cabin-cta">
+      <span>Cabin Info</span>
+      <img src="imgs/arrows.svg" alt="arrow" />
+    </a>
+  </li>
+  `;
+    })
+    .join("");
+
+  threeCabins.innerHTML = threeCabinsHtml;
+}
+
+// RENDER CABINS IN CABIN.HTML
+
+const allCabins = document.getElementById("featured-cabins");
+
+if (allCabins) {
+  const cabinsHtml = cabins
+    .map(({ image, name, shortDescription }) => {
+      return `
+    <li>
+      <a href="#"><img src="${image}" class="cabin-img" /></a>
+
+      <p class="cabin-title">${name}</p>
+      <p class="cabin-desc">
+        ${shortDescription}
+      </p>
+
+      <a href="#" class="cabin-cta">
+        <span class="black">Cabin Info</span>
+        <img src="imgs/arrows-black.svg" />
+      </a>
+    </li>
+`;
+    })
+    .join("");
+
+  allCabins.innerHTML = cabinsHtml;
+}
